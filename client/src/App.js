bec07d5ccd2ee4792ecf6419f805ec8070d3a7aa
umed tolibi <tolibiumed@gmail.com>
2018-09-20 20:19:46 -0400

    Redux implementation

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,18 @@ import React, { Component } from "react";
 import AppNavbar from "./components/AppNavbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ShoppingList from "./components/ShoppingList";
+import { Provider } from "react-redux";
+import store from "./store";
 import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppNavbar />
-        <ShoppingList />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <ShoppingList />
+        </div>
+      </Provider>
     );
   }
 }
